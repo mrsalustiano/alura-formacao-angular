b4f6310707cf16e9a2a55f06a88e75109444d8b9
Marcelo  Silva <mrsalustiano@gmail.com>
2021-05-16 01:33:45 -0300

    aula-1

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferecia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+
+})
+
+export class NovaTransferenciaComponent {
+
+}
+

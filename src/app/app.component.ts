77f798d2e78320ca9063cc11e5992dd9237345f1
Marcelo  Silva <mrsalustiano@gmail.com>
2021-05-21 01:02:37 -0300

    Projeto Finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,15 @@
+import { TransferenciaService } from './service/transferencia.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bytebank';
-  destino: number;
-  valor: number;
-
-  transferir($event){
-    console.log($event);
-    this.destino = $event.valorEmitir.destino;
-    this.valor = $event.valorEmitir.valor;
 
 
-  }
+  constructor(private service: TransferenciaService) {}
+
 }
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,31 +1,38 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-
+import { TransferenciaService } from './../service/transferencia.service';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Transferencia } from '../models/transferencia.model';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-nova-transferecia',
+  selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
   styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
-
-
-  @Output() Transferencia = new EventEmitter<any>();
+  @Output() aoTransferir = new EventEmitter<any>();
 
   valor: number;
   destino: number;
 
+  constructor(private service: TransferenciaService, private router: Router) {}
+
   // tslint:disable-next-line: typedef
   transferir() {
-    const valorEmitir = {valor: this.valor, destino: this.destino};
-    this.Transferencia.emit( {valorEmitir} );
+    console.log('Solicitada nova transferência');
+    const valorEmitir: Transferencia = { valor: this.valor, destino: this.destino };
 
-    this.limparCampos();
+    this.service.adicionar(valorEmitir).subscribe(resultado => {
+      console.log(resultado);
+      this.limparCampos();
+      this.router.navigateByUrl('extrato');
+    },
+    error => console.log(error));
+
   }
 
   // tslint:disable-next-line: typedef
   limparCampos(){
     this.valor = 0;
     this.destino = 0;
-
   }
 }
